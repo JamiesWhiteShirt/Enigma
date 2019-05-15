ad8c02b31a4bc3ec4f548fbb13adf64a0231a820
Erlend Åmdal <erlend@aamdal.com>
2019-05-15 22:35:03 +0200

    Avoid ArithmeticException on empty maps

--- src/main/java/cuchaz/enigma/analysis/index/ReferenceIndex.java
+++ src/main/java/cuchaz/enigma/analysis/index/ReferenceIndex.java
@@ -84,6 +84,9 @@ public class ReferenceIndex implements JarIndexer {
 	}
 
 	private <K extends Entry<?>, V extends Entry<?>> Multimap<K, V> remapReferences(JarIndex index, Multimap<K, V> multimap) {
+		if (multimap.isEmpty()) {
+			return HashMultimap.create();
+		}
 		Multimap<K, V> resolved = HashMultimap.create(multimap.keySet().size(), multimap.size() / multimap.keySet().size());
 		for (Map.Entry<K, V> entry : multimap.entries()) {
 			resolved.put(remap(index, entry.getKey()), remap(index, entry.getValue()));
@@ -92,6 +95,9 @@ public class ReferenceIndex implements JarIndexer {
 	}
 
 	private <E extends Entry<?>, C extends Entry<?>> Multimap<E, EntryReference<E, C>> remapReferencesTo(JarIndex index, Multimap<E, EntryReference<E, C>> multimap) {
+		if (multimap.isEmpty()) {
+			return HashMultimap.create();
+		}
 		Multimap<E, EntryReference<E, C>> resolved = HashMultimap.create(multimap.keySet().size(), multimap.size() / multimap.keySet().size());
 		for (Map.Entry<E, EntryReference<E, C>> entry : multimap.entries()) {
 			resolved.put(remap(index, entry.getKey()), remap(index, entry.getValue()));
